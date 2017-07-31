9a50b2a2e3004a4915f4fee24e80f0a39ac8a20c
Marina Merlo <marina27merlo@gmail.com>
2017-07-31 06:20:32 -0300

    Update graficos.R

--- graficos.R
+++ graficos.R
@@ -97,6 +97,107 @@ p <- ggplot(dados_coligacoes, aes(x = colig, y = votos_total_cand, label = nome_
   theme(axis.text.x=element_text(angle=40, hjust=1))
 ggsave("jitter_resultado_genero_votos_coligacao.png", width = 10, height = 5)
 
+
+### versões coloridas ###
+
+##gráfico de barras de candidaturas por partido e por gênero - colorido
+g <-ggplot(data = dados, aes(x = reorder(sigla, cand_part_fem_pct),  y = cand_part, fill = genero)) + 
+  geom_bar(stat = "identity", position = "fill") +
+  theme_minimal()+
+  scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
+  geom_hline(yintercept = 0.7) +
+  labs(title ="Gênero das candidaturas por Partido", x = "Partido", y = "% de candidaturas") + 
+  theme(axis.text.x=element_text(angle=50, hjust=1), 
+        legend.position = "bottom") +
+  coord_flip() +
+  scale_y_reverse(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                labels = c("100%","75%","50%", "25%", "0%"))
+  
+g
+ggsave("barplot_candidaturas_genero_partido_cor.png", width = 8, height = 5)
+
+
+kdensity <- ggplot(data=dados, aes(x=voto_total_cand.log, fill=factor(genero))) +
+  geom_density(alpha=0.4, size = 0.05, colour = "gray30") +
+  theme_minimal()+
+  scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
+  labs(title = "Densidade dos Votos",
+       x = "Votos Totais (log)",
+       y = NULL) + 
+  facet_grid(. ~eleito) + 
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5)) + 
+  scale_y_reverse() + 
+  coord_flip()
+kdensity
+
+boxplot <- ggplot(dados, aes(genero, voto_total_cand.log, fill=factor(genero))) +
+  theme_minimal()+
+  geom_boxplot(alpha=0.4, lwd=0.1, colour = "gray30")+
+  scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
+  facet_grid(. ~eleito) +
+labs(title = "Distribuição dos votos",
+                            y = "Votos Totais (log)",
+                            x = "Gênero") + 
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom")
+
+boxplot
+
+library(gridExtra)
+grid.arrange(kdensity, boxplot, ncol=1)
+ggsave("boxplot_kdensity_votos_resultado_genero.png", width = 5, height = 5, arrangeGrob(bplot2, bplot3, ncol=1))
+
+#tabela de estatísticas de votos por genero e resultado
+stats_resumo <- dados %>%
+  group_by(eleito, genero) %>%
+  summarise(soma_votos = sum(votos_total_cand, na.rm = TRUE),
+            media_votos = mean(votos_total_cand, na.rm = TRUE),
+            mediana_votos = median(votos_total_cand, na.rm = TRUE),
+            desvio_votos = sd(votos_total_cand, na.rm = TRUE),
+            minimo_votos = min(votos_total_cand, na.rm = TRUE),
+            maximo_votos = max(votos_total_cand, na.rm = TRUE))
+write.table(stats_resumo, "votos_stats.csv", row.names = F, sep = ";", dec =",")  
+
+#tabela de estatísticas por votos
+stats_votos <- dados %>%
+ group_by(genero) %>%
+summarise(soma_votos = sum(votos_total_cand, na.rm = TRUE),
+media_votos = mean(votos_total_cand, na.rm = TRUE),
+mediana_votos = median(votos_total_cand, na.rm = TRUE),
+desvio_votos = sd(votos_total_cand, na.rm = TRUE),
+minimo_votos = min(votos_total_cand, na.rm = TRUE),
+maximo_votos = max(votos_total_cand, na.rm = TRUE))
+
+#salvando
+stats_votos_eleitas <- eleitas %>%
+  group_by(nome_urna) %>%
+  summarise(soma_votos = sum(votos_total_cand, na.rm = TRUE))
+write.table(stats_votos_eleitas, "votos_eleitas.csv", row.names = F, sep = ";", dec =",") 
+
+#prepando a variável da coligação para ficar como fator, para o gráfico
+dados_coligacoes$colig <- as.factor(dados_coligacoes$colig)
+
+library(ggrepel)
+ggplot(data = dados_coligacoes, aes(x = colig, y = votos_total_cand, label = nome_urna)) +
+  theme_bw() +
+  scale_colour_manual(name = "Gênero", values = c("black","grey69")) +
+  scale_shape_manual(name="Resultado", values=c(18,4)) +
+  geom_jitter(data = dados_coligacoes, alpha = 0.7, aes(colour = factor(genero), size = votos_total_cand, shape = eleito)) +
+  labs(title ="Votação Por Coligação (somente que elegeram candidatos)", 
+       x = "Coligação", 
+       y = "Votos") + 
+  scale_size_continuous(name = "Número de votos",
+                        breaks = c(0, 1000, 10000, 50000, 100000),
+                        labels=c("Zero votos", "1 mil votos",  "10 mil votos", "50 mil votos", "100 mil votos")) +
+  geom_text_repel(data= eleitas, label = nome_urna, size=2) +
+  theme(axis.text.x=element_text(angle=40, hjust=1))
+p
+ggsave("jitter_resultado_genero_votos_coligacao.png", width = 10, height = 5)
+
 #versão colorida com fundo preto:
 p <- ggplot(dados_coligacoes, aes(x = colig, y = votos_total_cand, alpha=genero,label = nome_urna)) +
   theme_bw() +
