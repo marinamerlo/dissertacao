aceabff8da3e1ee3e6f32eb7d60b5071f4d10ae8
Marina Merlo <marina27merlo@gmail.com>
2017-07-31 05:50:07 -0300

    Update variaveis_recursos.R

--- variaveis_recursos.R
+++ variaveis_recursos.R
@@ -2,6 +2,10 @@
 #junta o banco de resultados e votos direto com o banco de recursos, sem fazer o aggregate e reshape
 #é melhor para fazer os gráficos de recursos!
 
+library(readr)
+library(tidyverse)
+library(data.table)
+
 ##########################
 ### TOTAL DE RECURSOS ###
 
@@ -195,3 +199,74 @@ dadosfull <- dadosfull %>%
   mutate(valor_tipo_candidatos_cand_pct = (valor_tipo_candidatos_cand / valor_total_cand)*100) %>%
   mutate(valor_tipo_partidos_cand_pct = (valor_tipo_partidos_cand / valor_total_cand)*100) %>%
   mutate(valor_tipo_pfisica_cand_pct = (valor_tipo_pfisica_cand / valor_total_cand)*100)
+
+
+############### Usando agora os dados de resultados por candidato (obs. = 1275) ############ 
+############### Fica mais fácil pra fazer os gráficos das vereadoras eleitas! ##############
+
+#banco de resultados e candidaturas
+dados2 <- fread("result_cand_SP.csv")
+#banco de receitas
+dados3 <- fread("receitas_2016.csv")
+
+#juntando os dois
+dados4 <- left_join(dados2, dados3, by ="seq")
+
+#renomeando algumas variáveis
+dados4 <- dados4 %>% 
+  mutate(genero = recode(genero, 
+                         "MASCULINO" = "Masculino", 
+                         "FEMININO" = "Feminino")) %>%
+  mutate(result = recode(result,
+                         "ELEITO POR MÉDIA" = "Eleito por média", 
+                         "ELEITO POR QP" = "Eleito por QP", 
+                         "NÃO ELEITO" = "Não Eleito", 
+                         "SUPLENTE" ="Suplente")) %>%
+  mutate(eleito = recode(result, 
+                         "Eleito por média" = "Eleito", 
+                         "Eleito por QP" = "Eleito", 
+                         "Suplente" = "Não Eleito",
+                         "Não Eleito" = "Não Eleito"))
+
+#criando o total de receita
+dados4 <- dados4 %>%
+  group_by(seq) %>%
+  mutate(valor_total_cand = sum(valor, na.rm=T))
+
+#criando o total de receita por coligação
+valor_colig <- dados %>%
+  group_by(colig) %>%
+  summarise(valor_total_colig = sum(valor_total_cand, na.rm=T))
+
+#criando o total de receita por partido
+valor_partido <- dados %>%
+  group_by(sigla) %>%
+  summarise(valor_total_partido = sum(valor_total_cand, na.rm=T))
+
+#juntando os dois valores acima no banco
+dados4 <- dados4 %>%
+  left_join(valor_colig, by ="colig") %>%
+  left_join(valor_partido, by ="sigla")
+
+#criando o total do fundo partidário recebido
+total_fundo <- dados4 %>%
+  filter(fonte == "Fundo Partidario") %>%
+  group_by(seq) %>%
+  summarise(valor_total_fundo_cand = sum(valor, na.rm=T))
+
+#juntando no banco
+dados4 <- dados4 %>%
+  left_join(total_fundo, by ="seq")
+
+#criando novas variáveis de porcentagem recebida de cada tipo de recurso ou origem
+dados4 <- dados4 %>%
+  mutate(pct_valor_total_partido_cand = valor_total_cand / valor_total_partido) %>%
+  mutate(pct_valor_total_colig_cand = valor_total_cand / valor_total_colig) %>%
+  mutate(pct_valor_total_fundo_cand = valor_total_fundo_cand / valor_total_cand) %>%
+  mutate(pct_valor_total_fundo_cand, 
+         pct_valor_total_fundo_cand = ifelse(is.na(pct_valor_total_fundo_cand) == TRUE, 0, pct_valor_total_fundo_cand))
+
+dados4$tipo <- ifelse(dados4$tipo == "Comercialização de bens ou realização de eventos" | dados4$tipo == "Recursos de origens não identificadas" |dados4$tipo == "Rendimentos de aplicações financeiras",
+                      "Outros", dados4$tipo)
+
+
