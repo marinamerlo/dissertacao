1831dd2d3a706bef85e49aced494586ff0e56c16
Marina Merlo <marina27merlo@gmail.com>
2017-08-01 21:42:01 -0300

    Update alluvial_conclusoes.R

--- alluvial_conclusoes.R
+++ alluvial_conclusoes.R
@@ -1,45 +1,16 @@
-devtools::install_github("mbojan/alluvial")
-
-
+install.packages("alluvial")
+install.packages("ggplot2")
 install.packages("data.table")
+install.packages("dplyr")
 
 library(alluvial)
 library(ggplot2)
 library(data.table)
 library(dplyr)
-
-tit <- as.data.frame(Titanic)
-
-# only two variables: class and survival status
-tit2d <- aggregate( Freq ~ Class + Survived, data=tit, sum)
-
-alluvial( tit2d[,1:2], freq=tit2d$Freq, xw=0.0, alpha=0.8,
-          gap.width=0.1, col= "steelblue", border="white",
-          layer = tit2d$Survived != "Yes" )
-
-alluvial( tit[,1:4], freq=tit$Freq, border=NA,
-          hide = tit$Freq < quantile(tit$Freq, .50),
-          col=ifelse( tit$Survived == "No", "red", "gray") )
-
-tit <- tibble::as_data_frame(Titanic)
-
-tit %>% head() %>% knitr::kable()
-
-alluvial(
-  select(tit, Survived, Sex, Age, Class),
-  freq=tit$n,
-  col = ifelse(tit$Class == "Crew", "orange", 
-  ifelse(tit$Class == "3rd", "red",
-  ifelse(tit$Class == "2nd", "black",
-  ifelse(tit$Class == "1st", "green", "grey")))),
-  border = ifelse(tit$Survived == "Yes", "orange", "grey"),
-  layer = tit$Survived != "Yes",
-  alpha = 0.8,
-  blocks=FALSE
-)
+library(ggalluvial)
 
 getwd()
-write.table(tit, "dados.csv", sep = ";", fileEncoding ="UTF-8", row.names = F)
+
 dados<- fread("dados.csv")
 
 dados$Ativ.Partidaria  = factor(dados$Ativ.Partidaria, levels=c("Próprio", "Família", "Mov. Estudantil"))
@@ -52,38 +23,30 @@ dados$Vereadora = factor(dados$Vereadora, levels=c("Janaína Lima (NOVO)",
                                                    "Juliana Cardoso (PT)",
                                                    "Isa Penna (PSOL)",
                                                    "Sâmia Bomfim (PSOL)"))
-
 ord <- list(NULL,NULL,NULL,
-            NULL,NULL,with(dados, order(partido)),
+            NULL,NULL,NULL,
             NULL)
-
-
 
 alluvial(
   select(dados, Vereadora, `Ativ.Politica`,`Ativ.Partidaria`,
          `Filiação`, `Decisão Candidatura`,`Apoio Candidatura`,`Capital Político`),
   freq=dados$n,
-  col = ifelse(dados$Vereadora == "Janaína Lima (NOVO)", "orange", 
-               ifelse(dados$Vereadora == "Adriana Ramalho (PSDB)", "blue",
-                      ifelse(dados$Vereadora == "Aline Cardoso (PSDB)", "black",
+  col = ifelse(dados$Vereadora == "Janaína Lima (NOVO)", "darkorange2", 
+               ifelse(dados$Vereadora == "Adriana Ramalho (PSDB)", "mediumblue",
+                      ifelse(dados$Vereadora == "Aline Cardoso (PSDB)", "royalblue4",
                              ifelse(dados$Vereadora == "Edir Sales (PSD)", "seagreen",
-                                    ifelse(dados$Vereadora == "Juliana Cardoso (PT)", "red",
-                                           ifelse(dados$Vereadora == "Sâmia Bomfim (PSOL)", "yellow",
-                                                  ifelse(dados$Vereadora == "Isa Penna (PSOL)", "yellow","grey"))))))),
-  alpha = 0.5,
+                                    ifelse(dados$Vereadora == "Juliana Cardoso (PT)", "firebrick1",
+                                           ifelse(dados$Vereadora == "Sâmia Bomfim (PSOL)", "yellow3",
+                                                  ifelse(dados$Vereadora == "Isa Penna (PSOL)", "yellow2","grey"))))))),
+  alpha = 0.7,
   blocks=TRUE,
-  border = ifelse(dados$Vereadora == "Janaína Lima (NOVO)", "orange", 
-                  ifelse(dados$Vereadora == "Adriana Ramalho (PSDB)", "blue",
-                         ifelse(dados$Vereadora == "Aline Cardoso (PSDB)", "black",
+  border = ifelse(dados$Vereadora == "Janaína Lima (NOVO)", "darkorange2", 
+                  ifelse(dados$Vereadora == "Adriana Ramalho (PSDB)", "mediumblue",
+                         ifelse(dados$Vereadora == "Aline Cardoso (PSDB)", "royalblue3",
                                 ifelse(dados$Vereadora == "Edir Sales (PSD)", "seagreen",
-                                       ifelse(dados$Vereadora == "Juliana Cardoso (PT)", "red",
-                                              ifelse(dados$Vereadora == "Sâmia Bomfim (PSOL)", "yellow",
-                                                     ifelse(dados$Vereadora == "Isa Penna (PSOL)", "yellow","grey"))))))),
-  #ordering=ord,
-  gap.width=0.1,
-  layer=dados$partido
-
-  
-)
-
-edit(dados)
+                                       ifelse(dados$Vereadora == "Juliana Cardoso (PT)", "firebrick1",
+                                              ifelse(dados$Vereadora == "Sâmia Bomfim (PSOL)", "yellow3",
+                                                     ifelse(dados$Vereadora == "Isa Penna (PSOL)", "yellow2","grey"))))))),
+  ordering=ord,
+  gap.width=0.15,
+  cex=0.1)
